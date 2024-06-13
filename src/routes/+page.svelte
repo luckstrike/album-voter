0199219ea56fab8933d343005addc4a9f495ae26
ag0711 <ag0711@tamu.edu>
2024-06-13 00:25:12 -0600

    Added in unplugin-icons + a right arrow to the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<div class="flex flex-row flex-grow bg-gray-800 items-center p-4">
+<!-- TODO: Make the landing page be able to have multiple different song quotes you can switch through -->
+<!--       Ideally pull from the most popular polls but for now just hard code a couple                -->
+
+<script lang="ts">
+    import MdiArrowRightBold from '~icons/mdi/arrow-right-bold';
+</script>
+
+<div class="flex flex-row flex-grow bg-gray-800 items-center p-8">
     <div class="flex flex-col p-4 text-white items-start">
         <h1 class="text-6xl font-bold bg-gradient-to-r from-blue-500 to-pink-700 inline-block text-transparent bg-clip-text leading-tight">
             Next Level Album Voting
@@ -9,12 +16,15 @@
         </div>
         
     </div>
-    <div class="flex flex-col flex-grow text-white p-4 items-end">
+    <div class="flex flex-col flex-grow text-white p-8 items-end">
         <img
             class="max-h-96 max-w-96 transform transition-transform duration-300 hover:rotate-3 hover:scale-110"
             src="https://i.discogs.com/rObvAZc7_iGhjPqHp2cQ_srbEmbm-7tjnW7ULLsfoGg/rs:fit/g:sm/q:90/h:600/w:592/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE2ODAx/OTc3LTE2MDk5MTEy/ODItMzYyMC5qcGVn.jpeg"
             alt="Album Cover for DOG by Kahlil Blu"
         />
         <p class="text-lg">runway talk - Kahlil Blu</p>
+    </div>
+    <div class="rounded-full bg-blue-500 p-1 text-white">
+        <MdiArrowRightBold style="font-size:2em"/>
     </div>
 </div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,4 +3,7 @@
 </script>
 
 <div class="flex flex-col flex-grow text-white bg-gray-800 p-4 items-center">
+    <div>
+        
+    </div>
 </div>

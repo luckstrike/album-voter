14a8ddb0cd40436fb26c4fafe8092ef9fc418b66
ag0711 <ag0711@tamu.edu>
2024-06-13 00:37:35 -0600

    Fixed up the landing page next quote button again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,8 @@
     import MdiArrowRightBold from '~icons/mdi/arrow-right-bold';
 </script>
 
-<div class="flex flex-row flex-grow bg-gray-800 items-center p-8">
-    <div class="flex flex-col p-4 text-white items-start">
+<div class="flex flex-row flex-grow bg-gray-800 items-center p-16 space-x-8">
+    <div class="flex flex-col text-white items-start">
         <h1 class="text-6xl font-bold bg-gradient-to-r from-blue-500 to-pink-700 inline-block text-transparent bg-clip-text leading-tight">
             Next Level Album Voting
         </h1>
@@ -14,9 +14,8 @@
         <div class="pt-10">
             <button class="rounded-full bg-blue-500 pl-4 pr-4 pt-2 pb-2 text-xl font-bold">Vote</button>
         </div>
-        
     </div>
-    <div class="flex flex-col flex-grow text-white p-8 items-end">
+    <div class="flex flex-col flex-grow text-white items-end">
         <img
             class="max-h-96 max-w-96 transform transition-transform duration-300 hover:rotate-3 hover:scale-110"
             src="https://i.discogs.com/rObvAZc7_iGhjPqHp2cQ_srbEmbm-7tjnW7ULLsfoGg/rs:fit/g:sm/q:90/h:600/w:592/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE2ODAx/OTc3LTE2MDk5MTEy/ODItMzYyMC5qcGVn.jpeg"
@@ -24,7 +23,7 @@
         />
         <p class="text-lg">runway talk - Kahlil Blu</p>
     </div>
-    <div class="rounded-full bg-blue-500 p-1 text-white">
+    <button class="rounded-full bg-blue-500 p-1 text-white">
         <MdiArrowRightBold style="font-size:2em"/>
-    </div>
+    </button>
 </div>

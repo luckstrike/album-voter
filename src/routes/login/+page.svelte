85d910becf88bd63b27c9e1596e322e3b9bd893d
ag0711 <ag0711@tamu.edu>
2024-06-12 23:43:35 -0600

    Created some stubs for the /vote and /login pages

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+
+</script>
+
+<div class="flex flex-col flex-grow text-white bg-gray-800 p-4 items-center">
+</div>
---
+++ src/routes/vote/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<div class="flex flex-col flex-grow text-white bg-gray-800 p-4 items-center">
+    <div class="text-6xl font-bold bg-gradient-to-r from-blue-500 to-pink-700 inline-block text-transparent bg-clip-text leading-tight">
+        Current Polls
+    </div>
+</div>

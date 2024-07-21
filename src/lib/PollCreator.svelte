30023178e6c2393ace676aff66bf5b9324e94ed1
ag0711 <ag0711@tamu.edu>
2024-07-21 02:47:35 -0500

    Albums in poll now look prettier, although if they overflow the div then stuff gets weird

--- src/lib/PollCreator.svelte
+++ src/lib/PollCreator.svelte
@@ -7,12 +7,24 @@
     export let albums: any[] = [];
 </script>
 
-<div class="flex flex-col bg-slate-900 rounded-lg items-center {className}">
-    <div class="p-2 text-2xl">Current Poll</div>
+<div class="flex flex-col bg-slate-900 rounded-lg p-4 items-center {className}">
+    <div class="text-2xl pb-2">Current Poll</div>
     {#if albums.length > 0}
         {#each albums as album}
             <!-- Make this look pretty now :) -->
-            <div>{album.name}</div>
+            <div
+                class="flex p-2 cursor-pointer mb-2 rounded-lg w-full bg-blue-500 hover:bg-gray-100"
+            >
+                <img
+                    src={album.images[2].url}
+                    alt={album.name}
+                    class="w-16 h-16 object-cover rounded flex-shrink-0"
+                />
+                <div class="flex flex-col justify-center p-2 min-w-0 flex-grow">
+                    <div class="font-semibold text-black truncate">{album.name}</div>
+                    <div class="text-sm text-gray-600 truncate">{album.artists[0].name}</div>
+                </div>
+            </div>
         {/each}
     {/if}
 </div>
